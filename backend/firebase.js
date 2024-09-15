dec9bbb40cc5a1c278f87689ac6b2e221339a926
Emmet-Ledell <131405384+Emmet-Ledell@users.noreply.github.com>
2024-09-15 05:25:42 -0400

    Removed api key

--- backend/firebase.js
+++ backend/firebase.js
@@ -4,15 +4,7 @@ const { getFirestore } = require("firebase/firestore");
 // const { getAnalytics } = require("firebase/analytics"); // Optional, only for browser environments
 
 // Your Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAIMNbU4PsyZj9LYNAmMQ_YhSYBb-UbGk8",
-  authDomain: "seefoodd-4f019.firebaseapp.com",
-  projectId: "seefoodd-4f019",
-  storageBucket: "seefoodd-4f019.appspot.com",
-  messagingSenderId: "825603661393",
-  appId: "1:825603661393:web:0f4384ba3e0d0581f96270",
-  measurementId: "G-2QZ88FX1SF"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
