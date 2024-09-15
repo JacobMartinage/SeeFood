3e070d6eb6c00eb16fc679289652e2aec6e62cdd
Emmet-Ledell <131405384+Emmet-Ledell@users.noreply.github.com>
2024-09-15 05:29:53 -0400

    Api key changes

--- backend/firebase.js
+++ backend/firebase.js
@@ -4,7 +4,14 @@ const { getFirestore } = require("firebase/firestore");
 // const { getAnalytics } = require("firebase/analytics"); // Optional, only for browser environments
 
 // Your Firebase configuration
-
+const firebaseConfig = {
+    authDomain: "seefoodd-4f019.firebaseapp.com",
+    projectId: "seefoodd-4f019",
+    storageBucket: "seefoodd-4f019.appspot.com",
+    messagingSenderId: "825603661393",
+    appId: "1:825603661393:web:0f4384ba3e0d0581f96270",
+    measurementId: "G-2QZ88FX1SF"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
